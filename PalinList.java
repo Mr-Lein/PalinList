620f5e0b0358a7313b16732b57504e91a3ef31b9
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-10-08 07:21:03 -0600

    Create PalinList.java

---
+++ PalinList.java
@@ -0,0 +1,10 @@
+// put your name here
+// PalinList Checker
+import java.util.Scanner;
+public class PalinList{
+  public static void main(String[] args){
+    //use a scanner to input the string from the keyboard
+    //use a combination of Stacks and Queues to determine if your string is a PALINLIST
+    //output a simple "YES" if it is a palinlist, "NO" if it is not.
+  }
+}
